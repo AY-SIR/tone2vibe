00b9c9baab339fdec1609fe2d1c0423fb14e3da9
AY-SIR <akkuyadav252006@gmail.com>
2025-09-22 09:40:57 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,18 @@ import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App.tsx";
 import EmailConfirmation from "./pages/EmailConfirmation.tsx";
+import { AuthProvider } from "./context/AuthContext.tsx"; // <-- add this
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/email-confirmation" element={<EmailConfirmation />} />
-        <Route path="/*" element={<App />} />
-      </Routes>
-    </BrowserRouter>
+    <AuthProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/email-confirmation" element={<EmailConfirmation />} />
+          <Route path="/*" element={<App />} />
+        </Routes>
+      </BrowserRouter>
+    </AuthProvider>
   </StrictMode>
 );
