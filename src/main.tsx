21fc059d4390ad51119f4741efc28a61cdf67fa3
AY-SIR <akkuyadav252006@gmail.com>
2025-09-25 09:27:50 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,1 +1,14 @@
-{"code":"rate-limited","message":"You have hit the rate limit. Please upgrade to keep chatting.","providerLimitHit":false,"isRetryable":true}
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import './index.css';
+
+// This is the entry point of your application
+createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
