cea9bcd9cc887a842f4d3451e7a221151b72a381
Akhilesh252006 <yadavakhilesh2519@gmail.com>
2025-10-17 09:09:58 +0530

    Update ConnectionStatus.tsx

--- src/components/common/ConnectionStatus.tsx
+++ src/components/common/ConnectionStatus.tsx
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useOfflineDetection } from '@/hooks/useOfflineDetection';
 
 export const ConnectionStatus: React.FC = () => {
-  // Run the hook to keep connection detection alive
-  useOfflineDetection();
+  const { isOffline, connectionQuality } = useOfflineDetection();
+  const [statusClass, setStatusClass] = useState('');
 
-  // Render nothing to the user
-  return null;
+  useEffect(() => {
+    if (isOffline) setStatusClass('offline');
+    else if (connectionQuality === 'poor') setStatusClass('poor');
+    else setStatusClass('online');
+
+    setTimeout(() => setStatusClass(''), 3000); // auto hide animation
+  }, [isOffline, connectionQuality]);
+
+  return <div className={`connection-anim ${statusClass}`} />;
 };
