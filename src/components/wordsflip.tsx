65fb7fe4c492a705545096ea7f4e49f151e23e99
Akhilesh252006 <yadavakhilesh2519@gmail.com>
2025-10-20 19:14:48 +0530

    py

--- src/components/wordsflip.tsx
+++ src/components/wordsflip.tsx
@@ -2,14 +2,15 @@
 
 import React from "react";
 import { FlipWords } from "@/components/ui/flip-words";
-
+import {
+  Mic,} from "lucide-react";
 export function WordsFlip() {
   const words = ["lively", "bright", "vivid"];
 
   return (
-    <section className="flex justify-center items-center px-4 py-8 mb-6">
+    <section className="flex justify-center items-center px-0 py-6 mb-6">
       <div className="max-w-5xl w-full">
-        <h1 className="text-left sm:text-center font-heading text-2xl sm:text-4xl md:text-5xl lg:text-6xl font-bold text-gray-700 dark:text-gray-100 leading-tight">
+        <h1 className="text-left sm:text-center font-heading text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-bold text-gray-700 dark:text-gray-100 leading-tight">
           <span className="block mb-2">
             Make your words{" "}
             <span className="inline-block">
@@ -17,7 +18,12 @@ export function WordsFlip() {
               <FlipWords words={words} />
             </span>
           </span>
-          <span className="block">with Tone2Vibe  . . . . .</span>
+        <span className="inline-flex items-center gap-4">
+  With
+  <Mic className="h-8 w-8 text-black" />
+</span>
+
+
         </h1>
       </div>
     </section>
