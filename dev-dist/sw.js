593270d072942d4b4fcc7f0c680862741f3cd962
Akhilesh252006 <yadavakhilesh2519@gmail.com>
2025-10-22 15:34:41 +0530

    akku

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.jlrvhkdrsh"
+    "revision": "0.mjik4p99m38"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
